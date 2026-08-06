c25529fdb296f0976b0afaf608864b2b0f17405d
Daniil Kirilyuk <daniel.kirilyuk@gmail.com>
2026-08-06 11:22:01 +0200

    NO-JIRA: [Broker-J] Stabilize intermittent system tests

--- bdbstore/systests/src/test/java/org/apache/qpid/server/store/berkeleydb/replication/MultiNodeTest.java
+++ bdbstore/systests/src/test/java/org/apache/qpid/server/store/berkeleydb/replication/MultiNodeTest.java
@@ -369,9 +369,8 @@ public class MultiNodeTest extends GroupJmsTestBase
         _failoverListener = new FailoverAwaitingListener();
         getJmsProvider().addGenericConnectionListener(connection, _failoverListener);
 
-        getBrokerAdmin().awaitRemoteNodeRole(activeBrokerPort, inactiveBrokerPort, "REPLICA");
-        Map<String, Object> attributes = getBrokerAdmin().getRemoteNodeAttributes(activeBrokerPort, inactiveBrokerPort);
-        assertEquals("REPLICA", attributes.get(BDBHAVirtualHostNode.ROLE),
+        final Object role = getBrokerAdmin().awaitRemoteNodeRole(activeBrokerPort, inactiveBrokerPort, "REPLICA");
+        assertEquals("REPLICA", role,
                      "Inactive broker has unexpected role");
 
         getBrokerAdmin().setRemoteNodeAttributes(activeBrokerPort,
@@ -381,7 +380,7 @@ public class MultiNodeTest extends GroupJmsTestBase
         _failoverListener.awaitFailoverCompletion(FAILOVER_COMPLETION_TIMEOUT);
         LOGGER.info("Listener has finished");
 
-        attributes = getBrokerAdmin().getNodeAttributes(inactiveBrokerPort);
+        final Map<String, Object> attributes = getBrokerAdmin().getNodeAttributes(inactiveBrokerPort);
         assertEquals("MASTER", attributes.get(BDBHAVirtualHostNode.ROLE), "Inactive broker has unexpected role");
 
         assumeTrue(Utils.produceConsume(connection, queue), "Message should be produced and consumed");
